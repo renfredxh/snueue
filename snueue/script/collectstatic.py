58a9e5b6d6e9c7b89a3f642ac8a9c1b3ebca9349
Renfred Harper <renfredxh@gmail.com>
2014-10-05 16:08:04 -0400

    Fix javascript asset path

--- snueue/script/collectstatic.py
+++ snueue/script/collectstatic.py
@@ -1,7 +1,7 @@
 from react import jsx
 
 def compile_jsx():
-    paths = [('assets/jsx/queue.jsx', 'assets/javascripts/queue.js')]
+    paths = [('assets/javascripts/queue.jsx', 'assets/javascripts/queue.js')]
 
     transformer = jsx.JSXTransformer()
     for jsx_path, js_path in paths:
