274a0be4f03950ebfe89dd9159704cc345309347
Renfred Harper <renfredxh@gmail.com>
2014-11-08 21:45:41 -0500

    Fix submission whitelist

--- snueue/reddit.py
+++ snueue/reddit.py
@@ -8,22 +8,47 @@ class SubmissionCollection(object):
     def __init__(self, submissions):
         self.submissions = [s for s in submissions]
 
+    @staticmethod
+    def media_type(submission):
+        """Determine the platform the submission links to based on its url"""
+        url = submission.url
+        types = {
+            'youtube': [r'youtube\.', 'youtu\.be']
+        }
+        for t, patterns in types.iteritems():
+            if any([re.search(p, url) for p in patterns]):
+                return t
+        return None
+
+    @staticmethod
+    def media_id(submission, sub_type):
+        sub_type = SubmissionCollection.media_type(submission)
+        if sub_type == 'youtube':
+            # Youtube video ids are an 11 digit base-64 encoded string usually
+            # embedded the form "v=<id>", "v%3D<id>" or "embed/<id>"
+            m = (re.search('(?<=v%3D)[A-Za-z0-9_-]{11}', submission.url) or
+                 re.search('(?<=v=)[A-Za-z0-9_-]{11}', submission.url) or
+                 re.search('[A-Za-z0-9_-]{11}', submission.url))
+            return m if m is None else m.group()
+
     def filter_self_posts(self):
         return SubmissionCollection([s for s in self.submissions if not s.is_self])
 
     def filter_media_type(self):
-        whitelist = ['tube', 'you']
+        """Return all submissions in the collection that have a supported
+        media type.
+        """
         return SubmissionCollection([
-            s for s in self.submissions if any([w in s.url for w in whitelist])
+            s for s in self.submissions if SubmissionCollection.media_type(s)
         ])
 
     def to_json(self):
         return [
             {
                 'id': submission.id,
-                'media_id': media_id(submission, type),
+                'media_id': SubmissionCollection.media_id(submission, type),
                 'title': submission.title,
-                'type': media_type(submission),
+                'type': SubmissionCollection.media_type(submission),
                 'url': submission.url,
                 'permalink': submission.permalink
             }
@@ -35,19 +60,6 @@ class SubmissionCollection(object):
     def __str__(self):
         return "SubmissionCollection({})".format(
                     str([s.id for s in self.submissions]))
-
-def media_type(submission):
-    return 'youtube'
-
-def media_id(submission, type):
-    type = media_type(submission)
-    if type == 'youtube':
-        # Youtube video ids are an 11 digit base-64 encoded string usually
-        # embedded the form "v=<id>", "v%3D<id>" or "embed/<id>"
-        m = (re.search('(?<=v%3D)[A-Za-z0-9_-]{11}', submission.url) or
-             re.search('(?<=v=)[A-Za-z0-9_-]{11}', submission.url) or
-             re.search('[A-Za-z0-9_-]{11}', submission.url))
-        return m.group()
 
 def get_fetch_method(subreddit, sorting):
     """Return the appropriate method for fetching submissions from a
