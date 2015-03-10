f6f53f11737331e3893d734d6f15f349377eb174
Renfred Harper <renfredxh@gmail.com>
2015-03-10 13:05:21 -0400

    Update error emails from address

--- snueue/config/production.py
+++ snueue/config/production.py
@@ -48,7 +48,7 @@ if ADMIN_EMAIL is not None:
     mail_user = get_secret('SNUEUE_LOGGING_MAIL_USER')
     mail_pass = get_secret('SNUEUE_LOGGING_MAIL_PASS')
     print mail_user, mail_pass
-    mail_handler = SMTPHandler((mail_server, 587), 'server-error@snueue.audio',
+    mail_handler = SMTPHandler((mail_server, 587), 'errors@snueue.audio',
                                [ADMIN_EMAIL], 'Snueue Server Error',
                                (mail_user, mail_pass))
     mail_handler.setLevel(logging.ERROR)
