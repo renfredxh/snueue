7532d955384f328c5204f6e420c909c1197d9996
Renfred Harper <renfredxh@gmail.com>
2015-06-04 22:09:45 -0400

    Add browserify filter for javacript assets

--- Dockerfile
+++ Dockerfile
@@ -13,14 +13,14 @@ RUN apt-get install -y tar git curl nano wget dialog net-tools build-essential s
 # Install Python and Basic Python Tools
 RUN apt-get install -y python3 python3-pip
 
-# Node for Babel and React Compiler
-RUN apt-add-repository ppa:chris-lea/node.js
-RUN apt-get update && apt-get install -y nodejs
-RUN npm install -g babel
-
 # Ruby and Compass
 RUN apt-get install -y -qq ruby1.9.1-dev make
 RUN gem install --no-rdoc --no-ri compass
+
+# Node for installing Browserify and Babel
+RUN apt-add-repository ppa:chris-lea/node.js
+RUN apt-get update && apt-get install -y nodejs
+RUN npm install -g browserify && npm install --save-dev babelify
 
 RUN git clone https://github.com/renfredxh/snueue.git snueue
 # Install the latest version of pip
--- snueue/assets.py
+++ snueue/assets.py
@@ -1,70 +1,37 @@
 import subprocess
 
 from flask.ext.assets import Bundle, Environment
-from webassets.filter import Filter, register_filter
+from webassets.filter import ExternalTool, register_filter
 from webassets.exceptions import FilterError
 
 from snueue import app
 
-class BabelFilter(Filter):
-    name = 'babel'
+class BrowserifyFilter(ExternalTool):
+    """Use Babelify and Browserify to transform and bundle javascript."""
+
+    name = 'browserify'
+    binary = name
     max_debug_level = None
 
-    def output(self, _in, out, **kwargs):
-        out.write(_in.read())
+    def input(self, infile, outfile, **kwargs):
+        transforms = ['babelify']
+        args = ['browserify']
 
-    def input(self, _in, out, **kwargs):
-        binary = 'babel'
-        try:
-            proc = subprocess.Popen([binary],
-                stdin=subprocess.PIPE,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE
-            )
-        except OSError as e:
-            if e.errno == 2:
-                raise Exception("babel not installed or in system path for webassets")
-            raise
-        stdout, stderr = proc.communicate(_in.read().encode('utf-8'))
-        if proc.returncode != 0:
-            raise FilterError('babel error: {}, {}'.format(
-                stderr.decode("utf-8"), stdout.decode("utf-8")
-            ))
-        elif stderr:
-            print("babel filter has warnings:", stderr.decode("utf-8"))
-        out.write(stdout.decode('utf-8'))
+        args.extend(['--transform'] + transforms)
+        args.append(kwargs['source_path'])
+        if app.config['DEBUG']:
+            args.append('--debug')
+
+        self.subprocess(args, outfile, infile)
 
 def bundle_stylesheets():
     css = Bundle(
         'stylesheets/app.scss',
         filters='compass',
         output='build/app.css',
-        depends='stylesheets/**/*.scss'
+        depends='stylesheets/**'
     )
     return css
-
-def bundle_es6():
-    files = [
-        'application.js',
-        'fullscreen.js'
-    ]
-    es6 = [
-        Bundle('javascripts/{}'.format(filename), filters='babel',
-               output='build/{}'.format(filename))
-        for filename in files
-    ]
-    return es6
-
-def bundle_jsx():
-    files = [
-        'queue.jsx'
-    ]
-    jsx = [
-        Bundle('javascripts/{}'.format(filename), filters='babel',
-               output='build/{}'.format(filename.replace('.jsx', '.js')))
-        for filename in files
-    ]
-    return jsx
 
 def bundle_javascripts():
     vendor = Bundle(
@@ -73,16 +40,20 @@ def bundle_javascripts():
         'javascripts/vendor/fixedsticky.js',
         'javascripts/vendor/react/react-with-addons.js',
     )
-    application = bundle_es6() + bundle_jsx()
+    application = Bundle(
+        'javascripts/application.js',
+        filters='browserify',
+        output='build/application.js',
+        depends=('javascripts/**')
+    )
     js = Bundle(
         vendor,
-        *application,
-        filters='rjsmin',
+        application,
         output='build/app.js'
     )
     return js
 
 assets = Environment(app)
-register_filter(BabelFilter)
+register_filter(BrowserifyFilter)
 assets.register('stylesheets', bundle_stylesheets())
 assets.register('javascripts', bundle_javascripts())
