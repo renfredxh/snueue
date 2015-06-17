24f7bc9174cf3f804a708dee79561ef29be20dc6
Renfred Harper <renfredxh@gmail.com>
2015-06-16 22:10:04 -0400

    Add index component

---
+++ snueue/assets/javascripts/components/Index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Index extends React.Component {
+  render() {
+    return (
+      <div className="logo-container">
+        <img src="assets/images/snueuelogo.svg"/>
+      </div>
+    );
+  }
+}
+
+export default Index;
--- snueue/assets/javascripts/routes.js
+++ snueue/assets/javascripts/routes.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Router, Route } from 'react-router';
+import { Router, Route, DefaultRoute } from 'react-router';
 
 import App from './components/App.jsx';
+import Index from './components/Index.jsx';
 import SubmissionSection from './components/SubmissionSection.jsx';
 
 const routes = (
   <Route handler={App}>
+    <DefaultRoute handler={Index}/>
     <Route name="subreddit" path="/r/:subreddit" handler={SubmissionSection} />
     <Route name="test" path="/s/:name" handler={SubmissionSection} />
   </Route>
